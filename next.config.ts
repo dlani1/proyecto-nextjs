6addf92c9e0684e342f4683b2fd83833e62801ef
dani1uwu <131733168+dani1uwu@users.noreply.github.com>
2025-05-30 18:25:12 -0600

    se agrega loading creado, tablas, busqueda y mutacion de datos

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+/*    experimental: {
+    ppr: 'incremental'
+  }
+   config options here */
 };
 
 export default nextConfig;
